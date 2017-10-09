2898e65c222c0d4b79868ad43e9c0ca5d3fbf1a5
Alain <vando026@umn.edu>
2017-10-09 06:32:00 +0200

    add config

---
+++ config.R
@@ -0,0 +1,8 @@
+# file:config.R
+# keep data_path empty for the default setting
+data_path <- "~/Dropbox/R/SuccessPlan/data"
+# Button labels must be stings
+Window_title <- "The Ultimate Success Plan"
+Button_label_1 <- "Project 1"
+Button_label_2 <- "Project 2"
+Button_label_3 <- "Wasted Time"
