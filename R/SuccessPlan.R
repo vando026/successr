6e183f3b4f6b158f2848941b3cab8b41b18accd6
Alain <vando026@umn.edu>
2017-11-05 12:17:50 +0200

    check works

--- R/SuccessPlan.R
+++ R/SuccessPlan.R
@@ -51,9 +51,9 @@ successr <- function(verbose=FALSE, sanitize=FALSE, ...) {
 
   options(guiToolkit="RGtk2" )
 
-  pkg_path <- system.file(package='successr')
-  # pkg_path <- dirname(getSrcDirectory(function(x) {x}))
+  pkg_path <- dirname(getSrcDirectory(function(x) {x}))
   data_path <- file.path(pkg_path, 'data')
+  if (!dir.exists(data_path)) dir.create(data_path)
 
   # Get configuration settings
   config_path <- file.path(pkg_path, "config.yml") 
