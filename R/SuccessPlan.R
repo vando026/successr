7cbe1303456a47730093037c8c1efeb1668aeb0b
Alain <vando026@umn.edu>
2019-07-02 12:36:31 +0200

    Changes to R load data

--- R/SuccessPlan.R
+++ R/SuccessPlan.R
@@ -414,6 +414,9 @@ load_daydata <- function(
   data_path=file.path(Sys.getenv("R_SUCCESS"), "DayData.csv")) {
   if (!file.exists(data_path)) 
     stop("You must set the R_SUCCESS environment variable to your data")
-  readCSV(data_path)
+  dat <- readCSV(data_path)
+  dat <- mutate(dat, 
+    Year = as.integer(format(Date, "%Y")),
+    Month = as.integer(format(Date, "%m")))
 }
 
